12e4c411891a07cc04bf0b24a3f928b077c57d54
Ali Taoube <ataoube00@stuy.edu>
2019-02-13 10:01:44 -0500

    Accidentally added to the wrong function

--- KnightBoard.java
+++ KnightBoard.java
@@ -53,17 +53,17 @@ public class KnightBoard{
   // @throws IllegalArgumentException when either parameter is negative
   //  or out of bounds.
   public boolean solve(int startingRow, int startingCol){
+    board[startingRow][startingCol] = 1;
 
+    return solveH(board.length, board[startingRow].length, 1);
   }
   //
   // @throws IllegalStateException when the board contains non-zero values.
   // @throws IllegalArgumentException when either parameter is negative
   //  or out of bounds.
-  public int countSolutions(int startingRow, int startingCol){
-    board[startingRow][StartingCol] = 1;
-
-    return solveH(board.length, board[startingRow].length, 1);
-  }
+  // public int countSolutions(int startingRow, int startingCol){
+  //
+  // }
 
   // Suggestion:
   private boolean solveH(int row ,int col, int level){
